fb7f867221d6bbf8d2ec95d22921eed21b76321d
Eduardo Quiñonez <okitoxd@gmail.com>
2016-02-25 19:49:52 -0500

    Version 1.0.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,37 +1,15 @@
 var button = document.getElementById('button-menu');
 var header = document.getElementById('header')
 var nav = document.getElementById('nav');
-
-var frases = ["¿Quieres comunicar?","¿Necesitas algo con que traer clientes?", "¿Quieres jugar?"];
+var items = document.getElementsByClassName('header-nav-item');
 
 var toggleMenu = function() {
    nav.classList.toggle('header-nav--show');
 	header.classList.toggle('header--show');
 }
 
-
-function maquina(contenedor,texto,intervalo){
-   // Calculamos la longitud del texto
-   longitud = texto.length;
-   // Obtenemos referencia del div donde se va a alojar el texto.
-   cnt = document.getElementById(contenedor);
-   var i=0;
-   // Creamos el timer
-   timer = setInterval(function(){
-      // Vamos añadiendo letra por letra y la _ al final.
-      cnt.innerHTML = cnt.innerHTML.substr(0,cnt.innerHTML.length-1) + texto.charAt(i) + "_";
-      // Si hemos llegado al final del texto..
-      if(i >= longitud){
-         // Salimos del Timer y quitamos la barra baja (_)
-         clearInterval(timer);
-         cnt.innerHTML = cnt.innerHTML.substr(0,longitud);
-         return true;
-      } else {
-         // En caso contrario.. seguimos
-         i++;
-      }},intervalo);
-};
-
-maquina("needs",frases[1],100);
-
 button.addEventListener("click", toggleMenu);
+
+for (var i = items.length - 1; i >= 0; i--) {
+   items[i].addEventListener("click", toggleMenu);
+}
