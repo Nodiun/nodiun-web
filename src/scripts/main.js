622b5260e53a54f6fe3b6cd2cfe222fc9b0d0567
Eduardo Quiñonez <okitoxd@gmail.com>
2016-04-12 15:43:30 -0500

    Added Google Analitycs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -13,3 +13,10 @@ button.addEventListener("click", toggleMenu);
 for (var i = items.length - 1; i >= 0; i--) {
    items[i].addEventListener("click", toggleMenu);
 }
+//Google analitycs
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-76351904-1', 'auto');
+ga('send', 'pageview');
